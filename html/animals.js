c7fd2fe18ccf0629cec3e8ad0488b317f803ae7b
andres <andrescorredor20@gmail.com>
2021-04-30 13:48:23 -0500

    add animation on click

--- html/animals.js
+++ html/animals.js
@@ -1,21 +1,37 @@
 const animals = [];
 
 for (let i = 0; i < 150; i++) {
-  var animalId = i % 3+1;
-  const animal = PIXI.Sprite.from("animal"+animalId+".png");
+  var animalId = (i % 3) + 1;
+  const animal = PIXI.Sprite.from("animal" + animalId + ".png");
   animal.anchor.set(0.5);
   container.addChild(animal);
+  animal.interactive = true;
+  // set the mousedown and touchstart callback...
+  animal
+    .on("mousedown", onButtonDown)
+    .on("touchstart", onButtonDown)
 
+    // set the mouseup and touchend callback...
+    .on("mouseup", onButtonUp)
+    .on("touchend", onButtonUp)
+    .on("mouseupoutside", onButtonUp)
+    .on("touchendoutside", onButtonUp)
+
+    // set the mouseover callback...
+    .on("mouseover", onButtonOver)
+
+    // set the mouseout callback...
+    .on("mouseout", onButtonOut);
   animal.direction = Math.random() * Math.PI * 2;
   let random = Math.random();
-  animal.speed = 0.2 + random*.7;
+  animal.speed = 0.2 + random * 0.7;
   animal.turnSpeed = 0.5;
 
   animal.x = Math.random() * bounds.width;
   animal.y = Math.random() * bounds.height;
 
   animal.scale.set(0.07 + Math.random() * 0.09);
-  animal.rateTinker = Math.random() * Math.PI * 2;
+  animal.rateTinker = 1;
   animal.original = new PIXI.Point();
   animal.original.copyFrom(animal.scale);
 
@@ -26,13 +42,12 @@ function animalAnimation(animals, bounds, count) {
     const animal = animals[i];
 
     animal.direction += animal.turnSpeed * 0.01;
-    animal.x += Math.sin(animal.direction) * animal.speed*2;
-    animal.y += Math.cos(animal.direction) * animal.speed*1;
+    animal.x += Math.sin(animal.direction) * animal.speed * 2;
+    animal.y += Math.cos(animal.direction) * animal.speed * 1;
 
     animal.rotation = -animal.direction - Math.PI / 2;
     animal.scale.x =
-      animal.original.x + Math.sin(count + animal.rateTinker) * 0.01;
-
+      animal.original.x + Math.sin(count * animal.rateTinker) * 0.03;
     if (animal.x < bounds.x) {
       animal.x += bounds.width;
     } else if (animal.x > bounds.x + bounds.width) {
@@ -45,4 +60,21 @@ function animalAnimation(animals, bounds, count) {
       animal.y -= bounds.height;
     }
   }
+}
+function onButtonDown() {
+  console.log(this.rateTinker);
+  this.rateTinker = this.rateTinker * 15;
+  this.speed = this.speed * 2;
+
+}
+
+function onButtonUp() {
+
+}
+
+function onButtonOver() {
+}
+
+function onButtonOut() {
+  this.rateTinker = 1;
 }
--- html/filters.js
+++ html/filters.js
@@ -1,30 +1,30 @@
-const ringDisplacementSprite = PIXI.Sprite.from("displace.png");
-const ringDisplacementFilter = new PIXI.filters.DisplacementFilter(
-  ringDisplacementSprite
-);
-container.addChild(ringDisplacementSprite);
+// const ringDisplacementSprite = PIXI.Sprite.from("displace.png");
+// const ringDisplacementFilter = new PIXI.filters.DisplacementFilter(
+//   ringDisplacementSprite
+// );
+// container.addChild(ringDisplacementSprite);
 
-var displacementSprite = new PIXI.Sprite.from("clouds.jpg");
-displacementSprite.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
-var displacementFilter = new PIXI.filters.DisplacementFilter(
-  displacementSprite
-);
-//container.addChild(displacementSprite);
-container.filters = [ringDisplacementFilter];
-ringDisplacementFilter.scale.x = 110;
-ringDisplacementFilter.scale.y = 110;
-ringDisplacementSprite.anchor.set(0.5);
-const ring = PIXI.Sprite.from("ring.png");
-ring.anchor.set(0.5);
-ring.visible = false;
-container.addChild(ring);
-container.on("mousemove", onPointerMove).on("touchmove", onPointerMove);
-function onPointerMove(eventData) {
-  ring.visible = true;
+// var displacementSprite = new PIXI.Sprite.from("clouds.jpg");
+// displacementSprite.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
+// var displacementFilter = new PIXI.filters.DisplacementFilter(
+//   displacementSprite
+// );
+// //container.addChild(displacementSprite);
+// container.filters = [ringDisplacementFilter];
+// ringDisplacementFilter.scale.x = 110;
+// ringDisplacementFilter.scale.y = 110;
+// ringDisplacementSprite.anchor.set(0.5);
+// const ring = PIXI.Sprite.from("ring.png");
+// ring.anchor.set(0.5);
+// ring.visible = false;
+// container.addChild(ring);
+// container.on("mousemove", onPointerMove).on("touchmove", onPointerMove);
+// function onPointerMove(eventData) {
+//   ring.visible = true;
 
-  ringDisplacementSprite.position.set(
-    eventData.data.global.x - 25,
-    eventData.data.global.y
-  );
-  ring.position.copyFrom(ringDisplacementSprite.position);
-}
+//   ringDisplacementSprite.position.set(
+//     eventData.data.global.x - 25,
+//     eventData.data.global.y
+//   );
+//   ring.position.copyFrom(ringDisplacementSprite.position);
+// }
