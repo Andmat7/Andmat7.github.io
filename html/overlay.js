9efcaa1d3a09ef60723aea73d2945ec8045555ac
andres <andrescorredor20@gmail.com>
2021-05-05 18:56:13 -0500

    update genomica

--- html/overlay.js
+++ html/overlay.js
@@ -4,9 +4,9 @@ logo.interactive = true;
 zoo.addChild(logo);
 logo.position.y = window.innerHeight - 60;
 logo.position.x = 12;
-logo.on("click", function () {
-  canvas.requestFullscreen();
-});
+  logo.on("click", function () {
+    canvas.requestFullscreen();
+  });
 
 let overlay = new PIXI.Container();
 overlay.width = window.innerWidth;
@@ -20,7 +20,7 @@ var BlurFilter = new PIXI.filters.BlurFilter(
 );
 
 // // Blurs whatever is rendered by the container
-overlay.filters = [BlurFilter];
+//zoo.filters = [BlurFilter];
 
 // Only the contents within a circle at the center should be rendered onto the screen.
 overlay.mask = new PIXI.Graphics()
@@ -34,6 +34,57 @@ let text = new PIXI.Text("SPECIES", {
   fill: "white",
   align: "center",
 });
+overlay.addChild(text);
+let text2 = new PIXI.Text("Gushi Larvae", {
+  fontFamily: "andale",
+  fontSize: 24,
+  fill: "white",
+  align: "center",
+});
+overlay.addChild(text2);
+let text3 = new PIXI.Text("Sushi/Snakes/Avacado/Eggs", {
+  fontFamily: "andale",
+  fontSize: 24,
+  fill: "white",
+  align: "center",
+});
+overlay.addChild(text3);
+
+let text4 = new PIXI.Text("Purple DNA Jelly", {
+  fontFamily: "andale",
+  fontSize: 24,
+  fill: "white",
+  align: "center",
+});
+overlay.addChild(text4);
+let text5 = new PIXI.Text("DNA/Cells/Jellyfish", {
+  fontFamily: "andale",
+  fontSize: 24,
+  fill: "white",
+  align: "center",
+});
+overlay.addChild(text5);
+let text6 = new PIXI.Text("White Blazer Bug", {
+  fontFamily: "andale",
+  fontSize: 24,
+  fill: "white",
+  align: "center",
+});
+overlay.addChild(text6);
+let text7 = new PIXI.Text("Skies/Cells/Ligtning", {
+  fontFamily: "andale",
+  fontSize: 24,
+  fill: "white",
+  align: "center",
+});
+overlay.addChild(text6);
+let text7 = new PIXI.Text("Skies/Cells/Ligtning", {
+  fontFamily: "andale",
+  fontSize: 24,
+  fill: "white",
+  align: "center",
+});
+overlay.addChild(text7);
 text.x = 100;
 overlay.addChild(text);
 container.addChild(overlay);
